e7b56e240b7774d00ac4b42b4d03f9c1391e2dae
shubhampachghare <shubhampachghare@gmail.com>
2022-12-12 23:42:45 +0530

    [shubham]- Created information of person

--- Program.cs
+++ Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 
-
 namespace AdressBookSystem
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book System!");
+            Console.WriteLine("Welcome To adress Book System!");
+            Program program = new Program();
+            Console.WriteLine("Enter name = ");
+            string Name = Console.ReadLine();
+           
+            Console.WriteLine("Enter address= ");
+            String address = Console.ReadLine();
+            Console.WriteLine("Enter city= ");
+            String city = Console.ReadLine();
+            Console.WriteLine("Enter state= ");
+            String state = Console.ReadLine();
+            Console.WriteLine("Enter zip= ");
+            String zip = Console.ReadLine();
+            Console.WriteLine("Enter phoneNumber= ");
+            String phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter email= ");
+            String email = Console.ReadLine();
         }
     }
- }
+}
---
+++ contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Address
+{
+    public string name;
+
+    public string address;
+    public string city;
+    public string state;
+    public string zip;
+    public string phoneNumber;
+    public string email;
+
+
+    public Address(string name, string address, string city, string state, string zip, string phoneNumber, string email)
+    {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}

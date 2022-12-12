327e7f14a0dcc9ac97531240668738ba0dafca3f
shubhampachghare <shubhampachghare@gmail.com>
2022-12-12 23:46:35 +0530

    [shubham]- Add new contact to address book

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace AdressBookSystem
         {
             Console.WriteLine("Welcome To adress Book System!");
             Program program = new Program();
-            Console.WriteLine("Enter name = ");
-            string Name = Console.ReadLine();
-           
+            Console.WriteLine("Enter first name = ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Enter last name = ");
+            string lName = Console.ReadLine();
             Console.WriteLine("Enter address= ");
             String address = Console.ReadLine();
             Console.WriteLine("Enter city= ");
@@ -23,6 +24,13 @@ namespace AdressBookSystem
             String phoneNumber = Console.ReadLine();
             Console.WriteLine("Enter email= ");
             String email = Console.ReadLine();
+
+            AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
+            adressBookBuilder.AddContact(fName, lName, address, city, state, zip, phoneNumber, email);
+            adressBookBuilder.displayContact();
+
+
+
         }
     }
 }
--- contact.cs
+++ contact.cs
@@ -1,25 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-class Address
+namespace AdressBookSystem
 {
-    public string name;
-
-    public string address;
-    public string city;
-    public string state;
-    public string zip;
-    public string phoneNumber;
-    public string email;
-
-
-    public Address(string name, string address, string city, string state, string zip, string phoneNumber, string email)
+    class Contact
     {
-        this.name = name;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+        public String fName;
+        public String lName;
+        public String address;
+        public String city;
+        public String state;
+        public String zip;
+        public String phoneNumber;
+        public String email;
+
+        public Contact(String fName, String lName, String address, String city, String state, String zip, String phoneNumber, String email)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+
+        }
+        public string toString()
+        {
+            return "first Name=" + fName + ",last name=" + lName + ",address="
+            + address + ",city=" + city + ",state=" + state + ",zip" +
+            zip + ",phone number=" + phoneNumber + "email=" + email;
+        }
     }
 }
